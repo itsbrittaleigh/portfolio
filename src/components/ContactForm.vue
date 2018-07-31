d5658419a3da59100425302b7eee9c8a895204a4
Britta Rodenbeck <rodenbeck.britta@gmail.com>
2018-07-30 20:13:56 -0400

    email validation

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -70,6 +70,10 @@ export default {
       if(this.errors.length === 0) this.handleSubmit();
       else e.preventDefault();
     },
+    validateEmail() {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(String(email).toLowerCase());
+    },
     encode(data) {
       return Object.keys(data)
         .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
