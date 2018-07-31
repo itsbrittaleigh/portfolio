5b036657db9c0d7615daf8383f126958d7726a8a
Britta Rodenbeck <rodenbeck.britta@gmail.com>
2018-07-31 09:53:28 -0400

    fixing email validation

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -70,7 +70,7 @@ export default {
       if(this.errors.length === 0) this.handleSubmit();
       else e.preventDefault();
     },
-    validateEmail() {
+    validateEmail(email) {
       var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       return re.test(String(email).toLowerCase());
     },
