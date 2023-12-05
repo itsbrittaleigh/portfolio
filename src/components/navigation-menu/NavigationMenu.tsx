83d8b3757dc5aa8ae2c24ac7e934a62ee5ac9ecd
Britta Rodenbeck <cheers@brittarodenbeck.com>
2023-12-05 12:38:30 -0600

    :lipstick: adding gradient links to navigation menu

--- src/components/navigation-menu/NavigationMenu.tsx
+++ src/components/navigation-menu/NavigationMenu.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './navigation-menu.css';
+import GradientLink from '../gradient-link/GradientLink';
 
 interface INavigationMenuProps {
   isOpen: boolean;
@@ -10,19 +11,18 @@ const NavigationMenu: React.FC<INavigationMenuProps> = ({ isOpen }) => {
     <nav className={`navigation-menu ${isOpen ? 'is-open' : ''}`}>
       <ul className="navigation-menu__list">
         <li>
-          <a className="navigation-menu__link" href="/about">About</a>
+          <GradientLink className="navigation-menu__link" href="/about" text="About" />
         </li>
         <li>
-          <a className="navigation-menu__link" href="/projects">Projects</a>
+          <GradientLink className="navigation-menu__link" href="/projects" text="Projects" />
         </li>
         <li>
-          <a
+          <GradientLink
             className="navigation-menu__link"
             href="/britta-rodenbeck__resume.pdf"
-            target="_blank"
-          >
-            Resume
-          </a>
+            newTab
+            text="Resume"
+          />
         </li>
       </ul>
     </nav>
